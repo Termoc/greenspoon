b23e5200aa218c7ed9062c4b443649f222b9b0c0
Termoc <termochubble@gmail.com>
2025-08-13 21:02:33 +0700

    add/change to local db 3

---
+++ components/MenuContent.tsx
@@ -0,0 +1,14 @@
+// app/menu/page.js
+import { Suspense } from "react";
+import MenuContent from "./MenuContent";
+
+export default function MenuPage() {
+  return (
+    <main>
+      <h1>Menu</h1>
+      <Suspense fallback={<div>Loading menu...</div>}>
+        <MenuContent />
+      </Suspense>
+    </main>
+  );
+}
